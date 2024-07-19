3ae25dc3ee173187b7f2d27253222b60d2b76517
Sakshi <sakshichoudhary000@gmail.com>
2024-07-19 23:55:23 +0530

    initial commit

---
+++ src/components/Display.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Display.module.css'
+function Display({displayValue}) {
+  return (
+    <input className={styles.display} type="text" value={displayValue} readOnly />
+  )
+}
+
+export default Display
